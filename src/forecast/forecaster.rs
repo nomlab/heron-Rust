24b44bc4cf27461de632add3b51dea2406f93540
nakazono0424 <nakazono2019@s.okayama-u.ac.jp>
2021-11-03 00:05:51 +0900

    Modified get bigwave cycle method

--- src/forecast/forecaster.rs
+++ src/forecast/forecaster.rs
@@ -1,18 +1,28 @@
 use chrono::prelude::*;
-use chrono::{Date, Duration, Utc};
+use chrono::{Date, Duration, Utc, Weekday};
+use jpholiday::jpholiday::JPHoliday;
 use ndarray::prelude::*;
 use ndarray_glm::{utility::standardize, Linear, ModelBuilder};
 use polars::prelude::*;
 
-fn weekdays(date: &Date<Utc>) -> Weekday {
-    date.weekday()
+fn weekdays(date: &Date<Utc>) -> String {
+    let jpholiday = JPHoliday::new();
+    if jpholiday.is_holiday(&date.naive_utc()) {
+        return "祝日".to_string();
+    }
+    match date.weekday() {
+        Weekday::Mon => return "月曜日".to_string(),
+        Weekday::Tue => return "火曜日".to_string(),
+        Weekday::Wed => return "水曜日".to_string(),
+        Weekday::Thu => return "木曜日".to_string(),
+        Weekday::Fri => return "金曜日".to_string(),
+        Weekday::Sat => return "土曜日".to_string(),
+        Weekday::Sun => return "日曜日".to_string(),
+    }
 }
 
 fn weekdays_considering_nholiday(dates: &Vec<Date<Utc>>) -> Vec<String> {
-    dates
-        .iter()
-        .map(|date| weekdays(&date).to_string())
-        .collect()
+    dates.iter().map(|date| weekdays(&date)).collect()
 }
 
 fn monthweek(date: &Date<Utc>) -> String {
@@ -27,6 +37,21 @@ fn months(dates: &Vec<Date<Utc>>) -> Vec<u32> {
     dates.iter().map(|date| date.month()).collect()
 }
 
+///////////////////////////////////////////
+// Generate parameters list
+//-----------------------------------------
+// dates  : date of vector
+// return : list of parameters
+///////////////////////////////////////////
+// Example
+//-----------------------------------------
+// dates  : vec!['2013/4/2', '2013/4/3', ...]
+// return :
+//           date  wday week month monthday
+//    ('2013/4/2',  Wed,   1,    4,       2
+//     '2013/4/3',  Thu,   1,    4,       3
+//      ...
+///////////////////////////////////////////
 fn get_params_list(dates: &Vec<Date<Utc>>) -> DataFrame {
     let wdays = weekdays_considering_nholiday(dates);
     let weeks = monthweeks(dates);
@@ -41,35 +66,46 @@ fn get_params_list(dates: &Vec<Date<Utc>>) -> DataFrame {
     plist
 }
 
-fn dates_to_occurreds(dates: &Vec<Date<Utc>>, range: &Vec<Date<Utc>>) -> Vec<u64> {
+fn dates_to_occurreds(dates: &Vec<Date<Utc>>, range: &Vec<Date<Utc>>) -> Vec<f64> {
     let len = (range[1] - range[0]).num_days() + 1;
-    let mut occurreds = vec![0; len as usize];
+    let mut occurreds = vec![0.0; len as usize];
 
     let seq_dates: Vec<Date<Utc>> = (0..len).map(|x| range[0] + Duration::days(x)).collect();
 
     for date in dates.iter() {
         for (j, seq_date) in seq_dates.iter().enumerate() {
             if date == seq_date {
-                occurreds[j] = 1;
+                occurreds[j] = 1.0;
             }
         }
     }
-
     occurreds
 }
 
-fn get_ac(f: &Vec<u64>, range: &Vec<Date<Utc>>) -> Vec<f64> {
+fn mean(v: &Vec<f64>) -> f64 {
+    let mut sum: f64 = 0.0;
+    for i in v {
+        sum += *i;
+    }
+    sum / v.len() as f64
+}
+
+fn get_ac(f: &Vec<f64>, range: &Vec<Date<Utc>>) -> Vec<f64> {
     // let start = 0;
-    let end = (range[1] - range[0]).num_days();
+    let end = (range[1] - range[0]).num_days() + 1;
 
     let mut ac: Vec<f64> = vec![0.0; (end + 1) as usize];
 
-    for lag in 0..=end {
+    for lag in 0..end {
         let mut p = Vec::new();
+        // f[lag] * f[0], f[lag + 1] * f[1], ..., f[f.len()] * f[f.len()]
+        // f[lag] * f[0], f[lag + 1] * f[1], ..., f[f.len()] * f[f.len() - lag]
         for i in 0..(f.len() as i64 - lag) {
-            p.push(f[i as usize] * f[lag as usize]);
+            p.push(f[(lag + i) as usize] * f[i as usize]);
         }
-        ac[lag as usize] = p.iter().sum::<u64>() as f64 / p.len() as f64;
+        if lag != 0 {
+            ac[(lag - 1) as usize] = mean(&p);
+        }
     }
 
     ac
@@ -78,28 +114,21 @@ fn get_ac(f: &Vec<u64>, range: &Vec<Date<Utc>>) -> Vec<f64> {
 fn get_big_wave_cycle(dates: &Vec<Date<Utc>>, range: &Vec<Date<Utc>>) -> usize {
     let series = dates_to_occurreds(dates, range);
     let mut ac = get_ac(&series, range);
+    let mut max = 0.0;
+    let mut max_index = 0;
 
     // 要修正
     // 長過ぎる周期をカット
     if ac.len() > 400 {
         ac = ac[..400].to_vec();
     }
-
-    // 最大値の index = 周期となるためこうしてるけどもっといい方法がありそう
-    let mut max = 0.0;
-    let mut max_index = 0;
-    for (i, &x) in ac.iter().enumerate() {
-        if x > max {
-            max = x;
-            max_index = i;
+    for (i, &val) in ac.iter().enumerate() {
+        if max < val {
+            max = val;
+            max_index = i + 1;
         }
     }
-
-    if ac.len() == 0 {
-        0
-    } else {
-        max_index
-    }
+    max_index
 }
 
 fn closest_event_index(events: &Vec<Date<Utc>>, date: Date<Utc>) -> usize {
@@ -249,17 +278,17 @@ pub fn forecast(
     range_candidate: &Vec<i64>,
     events: &Vec<Date<Utc>>,
 ) -> Date<Utc> {
+    // recurrence: 予定発生履歴
     let first = range_recurrence[0];
     let last = range_recurrence[1];
     let recurrence = events;
-    // let recurrence_plist = get_params_list(&recurrence);
+    let recurrence_plist = get_params_list(&recurrence);
 
-    // let mut period = get_big_wave_cycle(&recurrence, &range_recurrence);
-    // if period == 0 {
-    //     period = 365
-    // }
-
-    let period = 365;
+    // 次の予定の候補日
+    let mut period = get_big_wave_cycle(&recurrence, &range_recurrence);
+    if period == 0 {
+        period = 365;
+    }
 
     let candidates = get_candidates(&recurrence, &range_candidate, period);
     let candidates_plist = get_params_list(&candidates);
